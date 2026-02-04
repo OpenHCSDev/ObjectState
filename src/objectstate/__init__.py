c783d30a9c9e02409b3bffbbecd9f5ac80705831
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-02-03 22:32:45 -0500

    bump version to 1.0.14

--- src/objectstate/__init__.py
+++ src/objectstate/__init__.py
@@ -216,7 +216,7 @@ __all__ = [
     'CacheKey',
 ]
 
-__version__ = '1.0.13'
+__version__ = '1.0.14'
 __author__ = 'OpenHCS Team'
 __description__ = 'Generic configuration framework for lazy dataclass resolution'
 
