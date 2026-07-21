8444308fe68e90ea209922c2ed65a17cdf586465
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-07-21 15:04:07 -0400

    Release ObjectState 1.0.18

--- src/objectstate/__init__.py
+++ src/objectstate/__init__.py
@@ -256,7 +256,7 @@ __all__ = [
     'ObjectStateEditSession',
 ]
 
-__version__ = '1.0.17'
+__version__ = '1.0.18'
 __author__ = 'OpenHCS Team'
 __description__ = 'Generic configuration framework for lazy dataclass resolution'
 
