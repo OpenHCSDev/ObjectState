b0004daf3223326ac78689ba7b2e68abc18ae298
Tristan Simas <tristan.simas@mail.mcgill.ca>
2026-09-22 00:26:01 -0400

    Release 1.1.8 with schema-evolution reconciliation

--- src/objectstate/__init__.py
+++ src/objectstate/__init__.py
@@ -216,7 +216,7 @@ __all__ = [
     'ObjectStateEditSession',
 ]
 
-__version__ = "1.1.7"
+__version__ = "1.1.8"
 __author__ = 'OpenHCS Team'
 __description__ = 'Generic configuration framework for lazy dataclass resolution'
 
